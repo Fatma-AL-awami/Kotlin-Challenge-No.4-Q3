01425a4633e582a0e643f2950aebf989408796e5
fatma hassan <nihankamal2020@gmail.com>
2020-08-29 22:04:46 +0300

    retrieve the first and the last elements of the list without

---
+++ src/first and the last elements.kt
@@ -0,0 +1,13 @@
+fun main()
+{
+// one_methode
+    val numbers = listOf("one", "two", "three", "four", "five")
+    println(numbers.first())
+    println(numbers.last())
+
+// two method
+    println(numbers[0])
+    println(numbers[numbers.size-1])
+
+
+}
